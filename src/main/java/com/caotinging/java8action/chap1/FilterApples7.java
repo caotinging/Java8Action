a91ff22840337aeed79ed8b00e28f9873e3ab613
caotinging <caotinging@outlook.com>
2019-07-28 20:01:23 +0800

    first commit

---
+++ src/main/java/com/caotinging/java8action/chap1/FilterApples7.java
@@ -0,0 +1,14 @@
+package com.caotinging.java8action.chap1;
+
+/**
+ * @program: Java8Action
+ * @description: 使用java7的实现方法筛选苹果
+ * @author: CaoTing
+ * @create: 2019/7/28
+ */
+public class FilterApples7 {
+
+
+
+
+}
---
+++ src/main/java/com/caotinging/java8action/chap1/FilterApples8.java
@@ -0,0 +1,10 @@
+package com.caotinging.java8action.chap1;
+
+/**
+ * @program: Java8Action
+ * @description: 使用lambda实现筛选苹果
+ * @author: CaoTing
+ * @create: 2019/7/28
+ */
+public class FilterApples8 {
+}
